405071cba74cc90deab33f10b975bd89f427380e
LEON JOSEPH <ivanjoseph3030@gmail.com>
2024-06-26 14:15:20 +0530

    fix

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,13 +6,18 @@ function fetchEvents() {
             eventList.innerHTML = '';
             data.forEach(event => {
                 const li = document.createElement('li');
+                let eventClass = '';
                 if (event.type === 'PUSH') {
                     li.textContent = `${event.author} pushed to ${event.to_branch} on ${event.timestamp}`;
+                    eventClass = 'event-push';
                 } else if (event.type === 'PULL_REQUEST') {
                     li.textContent = `${event.author} submitted a pull request from ${event.from_branch} to ${event.to_branch} on ${event.timestamp}`;
+                    eventClass = 'event-pull-request';
                 } else if (event.type === 'MERGE') {
                     li.textContent = `${event.author} merged branch ${event.from_branch} to ${event.to_branch} on ${event.timestamp}`;
+                    eventClass = 'event-merge';
                 }
+                li.className = eventClass;
                 eventList.appendChild(li);
             });
         });
